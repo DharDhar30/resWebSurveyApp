9c02209a75e44de2dedea13298d8d5f1ab30ce6e
anjaliPillai20 <manoja7118@uhcl.edu>
2026-01-30 12:57:36 -0600

    Created color.jsx file and added the color combinations of the app

---
+++ src/color.jsx
@@ -0,0 +1,8 @@
+const colors = {
+  background: "#F5F5F5",   // Primary background
+  text: "#013364",          // Main dark blue
+  lightBlue: "#E1E8ED",     // Light blue accent
+  red: "#D30B0D",           // Red for button hover or alternate buttons
+};
+
+export default colors;
